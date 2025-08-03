6981d01c53035eb4a9838a85e2f4780e40942cf0
DESKTOP-E23IDKP\Abdullah Morsy <abdallamorsii@gmail.com>
2025-08-03 13:27:10 +0300

    new contact version is done and its working

---
+++ script.js
@@ -0,0 +1,14 @@
+// function sendEmail() {
+//     const templateParams = {
+//         name: document.querySelector("#name").value,
+//         email: document.querySelector("#email"),
+//         subject: document.querySelector("#subject").value,
+//         message: document.querySelector("#message").value,
+//     };
+
+//     emailjs.send("service_9s4uy1j", "template_jgm1tc3", templateParams)
+//     .then(() => alert("Email sent successfully!"))
+//     .catch((error) => alert("Failed to send email. Error: " + error));
+        
+
+// }
